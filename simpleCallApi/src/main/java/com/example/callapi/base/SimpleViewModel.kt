f535321026964c9d6cd333301fe1b872743c9a61
Oliveira Matheus <oliveira.matheus@ifood.com.br>
2021-03-05 19:36:03 -0300

    Add new properties to call.

--- simpleCallApi/src/main/java/com/example/callapi/base/SimpleViewModel.kt
+++ simpleCallApi/src/main/java/com/example/callapi/base/SimpleViewModel.kt
@@ -3,16 +3,25 @@ package com.example.callapi.base
 import androidx.lifecycle.ViewModel
 import com.example.callapi.ResultApi
 
-open class SimpleViewModel : ViewModel() {
+abstract class SimpleViewModel : ViewModel() {
+
+    private var onSuccess: ((Any?) -> Unit?) = {}
+    private var onError: (() -> Unit?) = {}
 
     suspend fun <T> T.safeCall(
-        call: suspend () -> ResultApi,
-        onSuccess: (Any?) -> Unit?,
-        onError: (() -> Unit?)?
+        call: suspend () -> ResultApi
     ) {
         when(val response = call.invoke()) {
             is ResultApi.Success -> { onSuccess(response.data) }
-            is ResultApi.Error -> { onError?.let { onError.invoke() } }
+            is ResultApi.Error -> { onError.let { onError.invoke() } }
         }
+    }
+
+    fun <T> T.onSuccess(action: ((Any?) -> Unit?)) {
+        onSuccess = action
+    }
+
+    fun <T> T.onError(action: (() -> Unit?)) {
+        onError = action
     }
 }
