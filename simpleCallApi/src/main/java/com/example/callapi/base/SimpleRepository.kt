3790516feea60b94bab1ef3b3aed424ccc4cf9ae
Oliveira Matheus <oliveira.matheus@ifood.com.br>
2021-03-27 15:39:28 -0300

    Change validation supress cast in collection

--- simpleCallApi/src/main/java/com/example/callapi/base/SimpleRepository.kt
+++ simpleCallApi/src/main/java/com/example/callapi/base/SimpleRepository.kt
@@ -7,25 +7,25 @@ import java.lang.Exception
 
 abstract class SimpleRepository {
 
-    suspend fun <T> safeCallApi(call: suspend () -> Response<T>) = safeResultApi(call)
+        suspend fun <T> safeCallApi(call: suspend () -> Response<T>) = safeResultApi(call)
 
-    private suspend fun <T> safeResultApi(call: suspend () -> Response<T>) : ResultApi {
-        return try {
-            val response = call.invoke()
+        private suspend fun <T> safeResultApi(call: suspend () -> Response<T>) : ResultApi {
+            return try {
+                val response = call.invoke()
 
-            if (response.isSuccessful) {
-                 ResultApi.Success(response.body())
-            } else {
-                val error = ErrorUtils.parserError(response)
+                if (response.isSuccessful) {
+                    ResultApi.Success(response.body())
+                } else {
+                    val error = ErrorUtils.parserError(response)
 
-                error?.let {
-                     ResultApi.Error(it.message, it.statusCode)
-                } ?: run {
-                     ResultApi.Empty("Retorno vazio")
+                    error?.let {
+                        ResultApi.Error(it.message, it.statusCode)
+                    } ?: run {
+                        ResultApi.Empty("Retorno vazio")
+                    }
                 }
+            } catch (e: Exception) {
+                ResultApi.Error()
             }
-        } catch (e: Exception) {
-             ResultApi.Error()
         }
     }
-}
--- simpleCallApi/src/main/java/com/example/callapi/base/SimpleViewModel.kt
+++ simpleCallApi/src/main/java/com/example/callapi/base/SimpleViewModel.kt
@@ -3,21 +3,22 @@ package com.example.callapi.base
 import androidx.lifecycle.ViewModel
 import com.example.callapi.ResultApi
 
-abstract class SimpleViewModel : ViewModel() {
+abstract class SimpleViewModel<T1> : ViewModel() {
 
-    private var onSuccess: ((Any?) -> Unit?) = {}
+    private var onSuccess: ((T1) -> Unit?) = {}
     private var onError: (() -> Unit?) = {}
 
+    @Suppress("UNCHECKED_CAST")
     suspend fun <T> T.safeCall(
         call: suspend () -> ResultApi
     ) {
         when(val response = call.invoke()) {
-            is ResultApi.Success -> { onSuccess(response.data) }
+            is ResultApi.Success -> { onSuccess(response.data as T1) }
             is ResultApi.Error -> { onError.let { onError.invoke() } }
         }
     }
 
-    fun <T> T.onSuccess(action: ((Any?) -> Unit?)) {
+    fun <T> T.onSuccess(action: ((T1) -> Unit?)) {
         onSuccess = action
     }
 
